e235ed98e303feb75d985492b93d17e6d9aa8787
Ahmed Eldesoky <ahmedeldesoky026@gmail.com>
2021-06-07 21:10:06 +0200

    edit

--- config/db.js
+++ config/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGO_URI, {
-      const conn = await mongoose.connect(
-        "mongodb+srv://ahmed5135:ahmed5135@eldesokyproshop.vazoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
